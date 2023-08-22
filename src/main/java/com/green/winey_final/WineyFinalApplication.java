4452493b5b367b580e91bdea429fc1241ac33fa3
배서윤 <qaz197@naver.com>
2023-08-22 15:02:58 +0900

    sy - yaml 임시설정

---
+++ src/main/java/com/green/winey_final/WineyFinalApplication.java
@@ -0,0 +1,13 @@
+package com.green.winey_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineyFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineyFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/winey_final/WineyFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.winey_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineyFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
