f0dc45f0300472d58eb999b97b8d280243f27153
정우진 <wash98@naver.com>
2023-08-24 12:13:15 +0900

    UserRecommendEntity생성완료

--- src/main/java/com/green/winey_final/common/entity/UserRecommendEntity.java
+++ src/main/java/com/green/winey_final/common/entity/UserRecommendEntity.java
@@ -1,4 +1,37 @@
 package com.green.winey_final.common.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.DynamicInsert;
+
+
+@Entity
+@Data
+@Table(name = "t_user_recommend")
+@SuperBuilder
+@NoArgsConstructor
+@ToString(callSuper = true)
+@DynamicInsert
 public class UserRecommendEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false, columnDefinition = "BIGINT UNSIGNED")
+    private Long userRecommendId;
+    @ManyToOne
+    @JoinColumn(name = "userId", updatable = false, nullable = false)
+    private UserEntity userEntity;
+    @Column(nullable = false, length = 20)
+    private Long categoryId;
+    @Column(nullable = false, length = 20)
+    private Long countryId;
+    @Column(nullable = false, length = 20)
+    private Long smallCateGoryId;
+    @Column(nullable = false, length = 20)
+    private Long priceRange;
+    @Column(nullable = false, length = 20)
+    private Long AromaCategoryId;
 }
