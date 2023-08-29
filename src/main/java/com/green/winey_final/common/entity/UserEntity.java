963c92ecba940472c81cb45a9ef973a91684f296
배서윤 <qaz197@naver.com>
2023-08-29 18:10:33 +0900

    sy - 로그인 로그아웃 확인

--- src/main/java/com/green/winey_final/common/entity/UserEntity.java
+++ src/main/java/com/green/winey_final/common/entity/UserEntity.java
@@ -82,6 +82,4 @@ public class UserEntity extends BaseEntity {
 
 
 
-    @Column
-    private String secretKey;
 }
--- src/main/java/com/green/winey_final/repository/UserTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.green.winey_final.repository;
-
-import com.green.winey_final.common.entity.UserId;
-import com.green.winey_final.common.entity.UserTokenEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserTokenRepository extends JpaRepository<UserTokenEntity, UserId> {
-}
