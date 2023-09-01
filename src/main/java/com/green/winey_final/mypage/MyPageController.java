2f98943058c5c60e00bc0a2598e8979fed96a984
배서윤 <qaz197@naver.com>
2023-09-01 20:00:48 +0900

    sy 2차->3차로 옮기는중

--- src/main/java/com/green/winey_final/mypage/MyPageController.java
+++ src/main/java/com/green/winey_final/mypage/MyPageController.java
@@ -1,6 +1,5 @@
 package com.green.winey_final.mypage;
 
-
 import com.green.winey_final.mypage.model.SelUserVo;
 import com.green.winey_final.mypage.model.UpduserDto2;
 import io.swagger.v3.oas.annotations.Operation;
@@ -29,14 +28,12 @@ public class MyPageController {
     }
 
 
-
     @DeleteMapping("/user-secession")
     @Operation(summary = "유저 삭제 처리",description =
             "주의사항: 로그인되어있을때 사용하셔야합니다. delYn이 0에서 1로 삭제처리로 바꾸는 작업입니다.<br>")
     public int delUser(Long userId){
         return service.delUser(userId);
     }
-
 
 
 }
