5b3560cdea16727440518326c3d057c76d2823f5
김원희 <kwh1619@naver.com>
2023-09-05 19:46:49 +0900

    wh get 하나로 통일...

--- src/main/java/com/green/winey_final/main/MainController.java
+++ src/main/java/com/green/winey_final/main/MainController.java
@@ -56,14 +56,14 @@ public class MainController {
             "\"cate\": [-] 카테고리(1: 레드, 2: 화이트 3: 스파클링 4: 기타),<br>" +
             "\"bigCate\": [-] 카테고리(1: meet, 2: seafood, 3: otherfood),<br>" +
             "\"country\": [-] 카테고리(1: 미국, 3: 프랑스, 4: 이탈리아, 6: 칠레 /그외 기타),<br>" +
-            "\"sort\": [1] 판매순 랭킹(0 : 최신순, 1: 높은가격순, 2: 낮은가격순)  <br>" +
+            "\"sort\": [1] 판매순 랭킹(0 : 오랜등록순, 0 : 최신등록순, 1: 높은가격순, 2: 낮은가격순)  <br>" +
             "\"price\": [1] 금액별 와인(0 : 2만원 이하, 1: 2~5만원 , 2: 5~10만원, 3: 10만원 이상)  <br>"
     )
     public WineCategoryDetailRes getSearchItem(@RequestParam(value = "cate",required=false)Long cate,
                                           @RequestParam(value = "bigCate",required=false)Long bigCate,
                                           @RequestParam(value = "country",required=false)Long country,
                                           @RequestParam(defaultValue = "1")int page,
-                                          @RequestParam(defaultValue = "9")int row,
+                                          @RequestParam(defaultValue = "6")int row,
                                           @RequestParam(defaultValue = "0")int sort,
                                           @RequestParam(defaultValue = "0")int price) {
 
